3675ee0610286073d72d5ed445b14adeb968c9a4
Hristo Koev <iamhristokoev@gmail.com>
2024-09-15 05:07:15 +0200

    feat: estimated earnings calc + mutation observer for starting up

--- script.js
+++ script.js
@@ -11,8 +11,8 @@
 (function () {
   "use strict";
 
-  // Function to find the session storage item with a specific environment
-  function findTokenKey() {
+  // Function to find the session storage item with a specific target
+  function findTokenKey({ target }) {
     const keys = Object.keys(sessionStorage);
 
     for (const key of keys) {
@@ -20,8 +20,12 @@
         // Retrieve and parse the item
         const item = JSON.parse(sessionStorage.getItem(key));
 
-        // Check if the item contains the environment
-        if (item && item.tokenType === "Bearer") {
+        // Check if the item contains the target
+        if (
+          item &&
+          item.tokenType === "Bearer" &&
+          item.target.includes(target)
+        ) {
           // Return the key.secret if available
           return item["secret"] || null;
         }
@@ -31,7 +35,7 @@
       }
     }
 
-    console.error("No item found with the specified environment");
+    console.error("No item found with the specified target");
     return null;
   }
 
@@ -65,7 +69,7 @@
 
   // Function to fetch data using the token from session storage
   async function fetchWithSessionToken(url) {
-    const token = findTokenKey();
+    const token = findTokenKey({ target: "/api" });
 
     if (!token) {
       console.error("No token found");
@@ -94,7 +98,7 @@
 
   // Function to renew the data when opening an asset
   async function renewData(filteredData) {
-    const token = findTokenKey();
+    const token = findTokenKey({ target: "/api" });
 
     if (!token) {
       console.error("No token found");
@@ -131,7 +135,7 @@
 
   async function fetchProgressData(assetPkIds) {
     const url = `/api/v2.0/AssetAdditionalData?type=2&assetPkIds=${assetPkIds}`;
-    const token = findTokenKey();
+    const token = findTokenKey({ target: "api" });
 
     if (!token) {
       console.error("No token found");
@@ -198,13 +202,11 @@
   }
 
   // Main function to handle the entire process
-  async function openAll() {
-    const url = "/api/asset/claimed";
-    const data = await fetchWithSessionToken(url);
+  async function openAll(data) {
+    if (!data) return;
+
     const assetPkIds = data.map((data) => data.assetPKID).join(",");
     const progressData = await fetchProgressData(assetPkIds);
-
-    if (!data) return;
 
     // Group and sort data by comment
     const filteredData = filterDataByProgress(data, progressData, false);
@@ -292,8 +294,109 @@
     });
   }
 
+  function roundToOneDecimal(value) {
+    return Math.ceil(value * 10) / 10;
+  }
+
+  // Function to calculate earnings for the month
+  async function calculateEarnings(providedData) {
+    const url = (from, to, quality) =>
+      `/api/v2/Metric/Volume?from=${from}&to=${to}&qualityLevel[0]=${quality}`;
+    const token = findTokenKey({ target: "/fluently-api" });
+
+    if (!token) {
+      console.error("No token found");
+      return null;
+    }
+
+    // Create a new Date object for the current date
+    const now = new Date();
+
+    // Extract the year and month from the current date
+    const year = now.getFullYear();
+    const month = now.getMonth(); // Note: month is zero-based (0 = January, 1 = February, etc.)
+
+    // Get today's date in YYYY-MM-DD format
+    const today = `${year}-${String(month + 1).padStart(2, "0")}-${String(
+      now.getDate()
+    ).padStart(2, "0")}`;
+
+    // Create a Date object for the first day of the current month
+    const firstDayOfMonth = new Date(year, month, 1);
+    const firstDay = `${firstDayOfMonth.getFullYear()}-${String(
+      firstDayOfMonth.getMonth() + 1
+    ).padStart(2, "0")}-${String(firstDayOfMonth.getDate()).padStart(2, "0")}`;
+
+    // Define the quality multipliers
+    const qualityMultipliers = [
+      { quality: "standard", multiplier: 0.787 },
+      { quality: "high", multiplier: 1.036 },
+    ];
+
+    // Process the provided data
+    const wordCountByQuality = providedData.reduce((acc, item) => {
+      const qualityKey = item.qualityName.toLowerCase();
+      if (!acc[qualityKey]) {
+        acc[qualityKey] = 0;
+      }
+      acc[qualityKey] += item.wordcount;
+      return acc;
+    }, {});
+
+    try {
+      // Fetch and aggregate data from the API
+      const apiDataPromises = qualityMultipliers.map(async ({ quality }) => {
+        const response = await fetch(url(firstDay, today, quality), {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const apiData = await response.json();
+        // Aggregate the API data
+        return {
+          quality,
+          wordcount: apiData.processedWords || 0,
+        };
+      });
+
+      const apiData = await Promise.all(apiDataPromises);
+
+      // Combine provided data and API data
+      apiData.forEach(({ quality, wordcount }) => {
+        const qualityKey = quality.toLowerCase();
+        if (!wordCountByQuality[qualityKey]) {
+          wordCountByQuality[qualityKey] = 0;
+        }
+        wordCountByQuality[qualityKey] += wordcount;
+      });
+
+      // Calculate the earnings
+      const results = qualityMultipliers.map(({ quality, multiplier }) => {
+        const processedWords = wordCountByQuality[quality] || 0;
+        return (roundToOneDecimal(processedWords) || 0) * multiplier;
+      });
+
+      // Sum up the results
+      const result = results.reduce((acc, value) => acc + value, 0);
+      return result;
+    } catch (error) {
+      console.error("Fetch error:", error);
+      return null;
+    }
+  }
+
   // Function to create and insert the custom button
-  function addCustomButton() {
+  async function addUi() {
+    const url = "/api/asset/claimed";
+    const data = await fetchWithSessionToken(url);
+
     // Select the buttons nav
     const buttonsNav = document.getElementById(
       "jobs-claimed-complete-button"
@@ -329,7 +432,7 @@
 
     // Add click event listener
     buttonOpenAll.addEventListener("click", function () {
-      openAll();
+      openAll(data);
     });
 
     buttonSelectCompleted.addEventListener("click", function () {
@@ -344,10 +447,91 @@
     buttonsNav.append(buttonOpenAll);
     buttonsNav.append(buttonSelectCompleted);
     buttonsNav.append(buttonInvert);
+
+    // Select the UI wordcount
+    const wordcountUiDiv = document.getElementById("jobs-claimed-wordcount");
+
+    // Create 'earnings' box UI
+    const strongElement = document.createElement("strong");
+    strongElement.className =
+      "ub-pl_8px ub-pr_8px ub-pb_8px ub-pt_8px ub-mt_8px ub-fnt-sze_16px ub-f-wght_600 ub-ln-ht_20px ub-ltr-spc_-0-05px ub-fnt-fam_b77syt ub-color_rgb102-120-138 ub-box-szg_border-box ub-bblr_4px ub-bbrr_4px ub-btlr_4px ub-btrr_4px";
+    strongElement.id = "jobs-claimed-total-wordcount-info";
+    strongElement.style.backgroundColor = "rgb(212, 238, 226)";
+
+    // Create the `span` element
+    const spanElement = document.createElement("span");
+
+    // Set the text content and style for the span
+    const earnings = await calculateEarnings(data);
+    spanElement.textContent = `${roundToOneDecimal(earnings)} CZK 🤑`;
+    spanElement.style.color = "rgb(71, 184, 129)";
+
+    // Append the `span` element to the `strong` element
+    strongElement.appendChild(document.createTextNode(`~EARNINGS: `));
+    strongElement.appendChild(spanElement);
+
+    // Append the `strong` element to the parent div
+    wordcountUiDiv.appendChild(strongElement);
   }
 
-  // Execute the function to add the button
-  setTimeout(() => {
-    addCustomButton();
-  }, 2000);
+  let hasRun = false; // Flag to ensure addUi runs only once
+
+  // Function to check if loading div is gone and then call addUi
+  function checkForLoadingDiv() {
+    const appRoot = document.querySelector("#app-root");
+    if (appRoot) {
+      const loadingDiv = appRoot.querySelector("#fluently-floading");
+      if (!loadingDiv) {
+        // Loading div is not present, wait a bit more to ensure UI is ready
+        setTimeout(() => {
+          if (hasRun) return; // Prevent running if already executed
+
+          // Re-check after a delay to ensure UI is ready
+          const contentReady = !appRoot.querySelector("#fluently-loading"); // Assuming no other loading indicators
+          if (contentReady) {
+            addUi();
+            hasRun = true; // Set flag to true after running
+            observer.disconnect(); // Stop observing once done
+          }
+        }, 2000); // Adjust the delay as needed
+      }
+    }
+  }
+
+  // Function to check for the specific button
+  function checkForButton() {
+    const button = document.querySelector("#jobs-claimed-start-button");
+    if (button) {
+      // Button is found, start checking for loading div
+      checkForLoadingDiv();
+      return true; // Stop further checking as we found the button
+    }
+    return false;
+  }
+
+  // Create a new MutationObserver instance
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        // Call the check function when there are changes in the children
+        if (checkForButton()) {
+          // Stop observing if the button has been found
+          observer.disconnect();
+        }
+      }
+    }
+  });
+
+  // Start observing the document body for changes
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  // Also check initially in case the element is already present
+  if (!checkForButton()) {
+    // If button isn't found initially, periodically check for it
+    const buttonCheckInterval = setInterval(() => {
+      if (checkForButton()) {
+        clearInterval(buttonCheckInterval); // Stop checking once the button is found
+      }
+    }, 500); // Adjust the interval as needed
+  }
 })();
